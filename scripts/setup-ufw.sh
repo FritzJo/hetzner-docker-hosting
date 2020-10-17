bf470d76c28ff8a63a6b8781b3cb0cc983474cbf
FritzJo <fritzjo-git@mailbox.org>
2020-10-18 00:30:02 +0200

    added ufw script

---
+++ scripts/setup-ufw.sh
@@ -0,0 +1,16 @@
+echo "Reset rules.."
+sudo ufw --force reset > /dev/null
+
+echo "Default deny incoming.."
+sudo ufw default deny incoming > /dev/null
+
+echo "Default deny outgoing.."
+sudo ufw default deny outgoing > /dev/null
+
+echo "Adding SSH.."
+sudo ufw allow 22 > /dev/null
+
+echo "Adding Webserver communication.."
+sudo ufw allow 80 > /dev/null
+sudo ufw allow 443 > /dev/null
+sudo ufw allow 9000 > /dev/null
