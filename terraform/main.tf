d9a7a23aac97145a3648e4738a3a300de7b8ea98
FritzJo <fritzjo-git@mailbox.org>
2020-11-26 23:14:31 +0100

    renamed vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,8 @@ data "hcloud_ssh_keys" "all_keys" {
 
 }
 # Create a server
-resource "hcloud_server" "wordpress-vps" {
-  name = "wordpress-vps"
+resource "hcloud_server" "hosting-vps" {
+  name = "hosting-vps"
   image = "debian-10"
   location = var.hcloud_location
   server_type = var.hcloud_server_type
@@ -20,19 +20,19 @@ resource "hcloud_server" "wordpress-vps" {
   provisioner "local-exec" {
 	  command = <<EOT
       sleep 30;
-	    >wordpress-instances.ini;
-	    echo "[wordpress]" | tee -a wordpress-instances.ini;
-	    echo "${hcloud_server.wordpress-vps.ipv4_address} ansible_user=root ansible_ssh_private_key_file=${var.private_key} floating_ip=${data.hcloud_floating_ip.floating-ip.ip_address}" | tee -a wordpress-instances.ini;
+	    >hosting-instances.ini;
+	    echo "[wordpress]" | tee -a hosting-instances.ini;
+	    echo "${hcloud_server.hosting-vps.ipv4_address} ansible_user=root ansible_ssh_private_key_file=${var.private_key} floating_ip=${data.hcloud_floating_ip.floating-ip.ip_address}" | tee -a hosting-instances.ini;
       export ANSIBLE_HOST_KEY_CHECKING=False;
-	    ansible-playbook -u root --private-key ${var.private_key} -i wordpress-instances.ini ../ansible/playbook-docker.yaml
-      ansible-playbook -u root --private-key ${var.private_key} -i wordpress-instances.ini ../ansible/playbook-hosting.yaml
+	    ansible-playbook -u root --private-key ${var.private_key} -i hosting-instances.ini ../ansible/playbook-docker.yaml
+      ansible-playbook -u root --private-key ${var.private_key} -i hosting-instances.ini ../ansible/playbook-hosting.yaml
       EOT
   }
 }
 
-resource "hcloud_floating_ip_assignment" "wordpress-ip" {
+resource "hcloud_floating_ip_assignment" "hosting-ip" {
   floating_ip_id = data.hcloud_floating_ip.floating-ip.id
-  server_id = hcloud_server.wordpress-vps.id
+  server_id = hcloud_server.hosting-vps.id
 }
 
 
