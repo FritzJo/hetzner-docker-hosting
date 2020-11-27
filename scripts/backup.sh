05a0a8b49d6fd084f8b99ac6e549e7f3cf267119
FritzJo <fritzjo-git@mailbox.org>
2020-11-27 18:41:57 +0100

    added backup to cron

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -13,8 +13,8 @@ fi
 export GOOGLE_APPLICATION_CREDENTIALS="/hosting/secrets/gcp-secret.json"
 export GOOGLE_PROJECT_ID="{{ GCP_Project_ID }}"
 export RESTIC_PASSWORD="{{ GCP_Backup_Password }}"
+
 # Backup for instance data + databases
-# restic -r gs:hosting-private-storage:/docker init
 echo "Backup instance data"
 
 if eval "$backup_command"; then
