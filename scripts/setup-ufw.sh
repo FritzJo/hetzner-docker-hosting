01b6dd244f85b958c8add87cae405de58824bce7
FritzJo <fritzjo-git@mailbox.org>
2021-05-07 21:56:59 +0200

    move ufw config to seperate file

--- scripts/setup-ufw.sh
+++ scripts/setup-ufw.sh
@@ -5,6 +5,17 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run this backup script as root"
   exit
 fi
+
+apply_external_rules () {
+    FILENAME=$1
+    echo "Processing $FILENAME"
+
+    while read -r line; do
+        echo "ufw $line"
+        eval "$(ufw "$line")"
+        done < "$FILENAME"
+}
+
 echo "Reset rules.."
 ufw --force reset > /dev/null
 
@@ -14,17 +25,6 @@ ufw default deny incoming > /dev/null
 echo "Default deny outgoing.."
 ufw default deny outgoing > /dev/null
 
-echo "Adding SSH.."
-ufw allow 22 > /dev/null
-
-echo "Adding DNS.."
-ufw allow out dns > /dev/null
-
-echo "Adding Webserver communication.."
-ufw allow out http > /dev/null
-ufw allow out https > /dev/null
-
-echo "Adding Portainer.."
-ufw allow 9000 > /dev/null
+apply_external_rules "../ansible/security/ufw-rules.conf"
 
 echo y | ufw enable
