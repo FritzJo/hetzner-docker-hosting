78efcee6e6469e57fd1f8c23c331b4845bdd73d2
FritzJo <fritzjo-git@mailbox.org>
2021-12-02 18:12:27 +0100

    floating ip is now optional.. (for real this time)

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ terraform {
 
 # Data
 data "hcloud_floating_ip" "floating-ip" {
+  count = var.floating_ip ? 1 : 0
   name=var.hcloud_floating_ip
 }
 
@@ -28,16 +29,18 @@ resource "hcloud_server" "hosting-vps" {
       sleep 30;
 	    >hosting-instances.ini;
 	    echo "[hosting-vps]" | tee -a ../custom/hosting-instances.ini;
-	    echo "${hcloud_server.hosting-vps.ipv4_address} floating_ip=${data.hcloud_floating_ip.floating-ip.ip_address}" | tee -a ../custom/hosting-instances.ini;
-            export ANSIBLE_HOST_KEY_CHECKING=False;
-            cd ../ansible || exit 1
+	    echo "${hcloud_server.hosting-vps.ipv4_address}" | tee -a ../custom/hosting-instances.ini;
+	    echo "[hosting-vps:vars]" | tee -a ../custom/hosting-instances.ini;
+      echo "floating_ip=${var.floating_ip ? data.hcloud_floating_ip.floating-ip[0].ip_address : ""}" | tee -a ../custom/hosting-instances.ini;
+      export ANSIBLE_HOST_KEY_CHECKING=False;
+      cd ../ansible || exit 1
 	    ansible-playbook master.yaml
       EOT
   }
 }
 
 resource "hcloud_floating_ip_assignment" "hosting-ip" {
-  floating_ip_id = data.hcloud_floating_ip.floating-ip.id
+  floating_ip_id = data.hcloud_floating_ip.floating-ip[0].id
   server_id = hcloud_server.hosting-vps.id
   count = var.floating_ip ? 1 : 0
 }
